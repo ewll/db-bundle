96f979d7d6a220925946604810907dd99f817ff8
Maxim Salamakhin <343604@gmail.com>
2020-08-13 11:38:31 +0700

    fix cache

--- DB/CacheKeyCompiler.php
+++ DB/CacheKeyCompiler.php
@@ -4,18 +4,10 @@ use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class CacheKeyCompiler
 {
-    private $projectCacheDir;
-
-    public function __construct(string $projectCacheDir)
-    {
-        $this->projectCacheDir = $projectCacheDir;
-    }
-
     public function compile(string $entityClass)
     {
-        $dirHash = hash('md5', $this->projectCacheDir);
         $entityKey = strtolower(preg_replace('/[^a-z]/i', '', $entityClass));
-        $key = sprintf('ewll.%s.entity.%s', $dirHash, $entityKey);
+        $key = sprintf('ewll.entity.%s', $entityKey);
 
         return $key;
     }
